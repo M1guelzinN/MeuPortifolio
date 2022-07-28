5bbeef03e680f9cda85732f42697c5222591d471
M1guelzinN <antonio.jobs1397@gmail.com>
2022-07-28 16:45:58 -0300

    cleancode

---
+++ complementos/js/lineHeight.js
@@ -0,0 +1,9 @@
+(function(){
+    const progress = document.getElementById('progressbar')
+    const totalHeight = document.body.scrollHeight - window.innerHeight
+
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100
+    progress.style.height = progressHeight + "%"
+}
+})()
--- complementos/js/menu.js
+++ complementos/js/menu.js
@@ -28,11 +28,4 @@
         menu.style.display = 'none'
       }
   }
-  let progress = document.getElementById('progressbar')
-let totalHeight = document.body.scrollHeight - window.innerHeight
-
-window.onscroll = function(){
-    let progressHeight = (window.pageYOffset / totalHeight) * 100
-    progress.style.height = progressHeight + "%"
-}
 })()
