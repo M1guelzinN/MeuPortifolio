6576f3dfae28ed83a9f9cdbd2b35301b6e45c9ad
M1guelzinN <antonio.jobs1397@gmail.com>
2022-07-28 12:38:51 -0300

    progressBar

--- complementos/js/menu.js
+++ complementos/js/menu.js
@@ -28,4 +28,11 @@
         menu.style.display = 'none'
       }
   }
+  let progress = document.getElementById('progressbar')
+let totalHeight = document.body.scrollHeight - window.innerHeight
+
+window.onscroll = function(){
+    let progressHeight = (window.pageYOffset / totalHeight) * 100
+    progress.style.height = progressHeight + "%"
+}
 })()
