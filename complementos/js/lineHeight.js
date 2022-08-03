853891f70a4394a5c24a556e789492c169248df3
M1guelzinN <antonio.jobs1397@gmail.com>
2022-08-03 12:00:30 -0300

    melhorias nas seções
    adicionei
    -links
    -skills
    -favicon
    +css

--- complementos/js/lineHeight.js
+++ complementos/js/lineHeight.js
@@ -1,8 +1,8 @@
 (function(){
-    const progress = document.getElementById('progressbar')
-    const totalHeight = document.body.scrollHeight - window.innerHeight
+  const progress = document.getElementById('progressbar')
+  const totalHeight = document.body.scrollHeight - window.innerHeight
 
-window.onscroll = function(){
+  window.onscroll = function(){
     let progressHeight = (window.pageYOffset / totalHeight) * 100
     progress.style.height = progressHeight + "%"
 }
